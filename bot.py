1e94106f379436dd636c8f59636b52c43e10c629
Belkacem Berchiche <b_berchiche@esi.dz>
2022-04-01 10:53:32 +0200

    3 legged auth

--- bot.py
+++ bot.py
@@ -1,14 +1,26 @@
 import tweepy
+import os
+from dotenv import load_dotenv
 
-def get_random_line(afile):
-    lines = open(afile).read().splitlines()
-    return random.choice(lines)
+load_dotenv()
 
-def tweet_random_line(afile):
-    status = get_random_line(afile)
-    api.update_status(status)
+API_KEY = os.getenv("API_KEY")
+API_KEY_SECRET = os.getenv("API_KEY_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
-    # Search for user mentions
-    for mention in api.mentions_timeline():
-        print(mention.text)
-        tweet_random_line("bouqala.csv")
+oauth1_user_handler = tweepy.OAuth1UserHandler(
+    API_KEY, API_KEY_SECRET,
+    callback="oob"
+)
+
+print(oauth1_user_handler.get_authorization_url())
+
+verifier = input("Input PIN: ")
+
+access_token, access_token_secret = oauth1_user_handler.get_access_token(
+    verifier
+)
+
+print("Access token:", access_token)
+print("Access token secret:", access_token_secret)
